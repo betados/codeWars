349ab067f5e6c56a99bbc8bff05fbef6c323ed47
wueato03 <alvaro.torres@entelgy.com>
2017-09-20 14:21:36 +0200

    first commit

--- reversePolishNotationCalculator.py
+++ reversePolishNotationCalculator.py
@@ -1,4 +1,5 @@
 def calc(expr):
+    print(expr)
     if len(expr) == 0:
         return 0
     
@@ -14,19 +15,23 @@ def calc(expr):
         pass
     while i < len(chain)-1 and len(chain)>0:
         i += 1
-        print(chain, i)
+        # print(chain, i)
         if chain[i] not in operators:
             continue
         if i==1:
             chain.pop(i)
-            chain.pop(i-1)
+            # chain.pop(i-1)
             i = -1
             continue
-        last = operatorsDict[ chain[i] ](int(chain[i-2]), int(chain[i-1]))
+        # print(chain[i-2] , chain[i-1])
+        print(chain)
+        last = operatorsDict[ chain[i] ](float(chain[i-2]), float(chain[i-1]))
         chain[i-1] = last
+        chain.pop(i)
         chain.pop(i-2)
         i = -1
-        print('pop:' ,chain)
+        # print('pop:' ,chain)
+    print(last)
     return last
             
 def add(a, b):
